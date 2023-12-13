ecedf419e6479168fb6d14691bda787ab1e17b6a
Mark Rawlins <mark.jeffrey.rawlins@gmail.com>
2023-12-13 22:05:51 +0900

    complete first draft header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -13,22 +13,26 @@ export function Header({header, isLoggedIn, cart}) {
   return (
     <header className="header">
       <div className="header-top">
-        <SearchToggle />
-        <NavLink prefetch="intent" to="/" end>
-          <img
-            className="logo-in-header"
-            src={img_url}
-            alt="logo of a well-groomed man in a coin"
-          />
-        </NavLink>
-        <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+        <div className="header-top__content">
+          <SearchToggle />
+          <NavLink prefetch="intent" to="/" end>
+            <img
+              className="logo-in-header"
+              src={img_url}
+              alt="logo of a well-groomed man in a coin"
+            />
+          </NavLink>
+          <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+        </div>
       </div>
       <div className="header-bottom">
-        <HeaderMenu
-          menu={menu}
-          viewport="desktop"
-          primaryDomainUrl={header.shop.primaryDomain.url}
-        />
+        <div className="header-bottom__content">
+          <HeaderMenu
+            menu={menu}
+            viewport="desktop"
+            primaryDomainUrl={header.shop.primaryDomain.url}
+          />
+        </div>
       </div>
     </header>
   );
@@ -96,12 +100,28 @@ export function HeaderMenu({menu, primaryDomainUrl, viewport}) {
 /**
  * @param {Pick<HeaderProps, 'isLoggedIn' | 'cart'>}
  */
+
+function accountIcon() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="20"
+        width="20"
+        viewBox="0 0 448 512"
+      >
+        <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+      </svg>
+    </>
+  );
+}
+
 function HeaderCtas({isLoggedIn, cart}) {
   return (
     <nav className="header-ctas" role="navigation">
       <HeaderMenuMobileToggle />
       <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
-        {isLoggedIn ? 'Account' : 'Sign in'}
+        {isLoggedIn ? 'Account' : accountIcon()}
       </NavLink>
       <CartToggle cart={cart} />
     </nav>
@@ -117,14 +137,37 @@ function HeaderMenuMobileToggle() {
 }
 
 function SearchToggle() {
-  return <a href="#search-aside">Search</a>;
+  return (
+    <a href="#search-aside">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="16"
+        width="16"
+        viewBox="0 0 512 512"
+      >
+        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+      </svg>
+    </a>
+  );
 }
 
 /**
  * @param {{count: number}}
  */
 function CartBadge({count}) {
-  return <a href="#cart-aside">Cart {count}</a>;
+  return (
+    <a href="#cart-aside">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="22"
+        width="22"
+        viewBox="0 0 512 512"
+      >
+        <path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z" />
+      </svg>
+      <div className="cart-aside__text">{count}</div>
+    </a>
+  );
 }
 
 /**
